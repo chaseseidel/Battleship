be30dab055c2065463abd8464a3c4d83328ee483
chaseseidel <chaseseidel16@gmail.com>
2023-07-30 19:36:53 -0700

    Change hit inputs

--- src/__tests__/ship.test.js
+++ src/__tests__/ship.test.js
@@ -9,44 +9,42 @@ beforeEach(() => {
 });
 
 test("Ships taking multiple hits", () => {
-  battleship.hit(1);
-  battleship.hit(2);
-  submarine.hit(1);
-  submarine.hit(2);
+  battleship.hit([1, 3]);
+  battleship.hit([1, 4]);
+  submarine.hit([4, 7]);
+  submarine.hit([4, 8]);
 
   expect(battleship.hits.length).toBe(2);
   expect(submarine.hits.length).toBe(2);
 });
 
 test("Ships not being sunk", () => {
-    battleship.hit(1);
-    battleship.hit(2);
-    submarine.hit(1);
-    submarine.hit(2);
-    expect(battleship.isSunk()).toBeFalsy();
-    expect(submarine.isSunk()).toBeFalsy();
+  battleship.hit([1, 3]);
+  battleship.hit([1, 4]);
+  submarine.hit([4, 7]);
+  submarine.hit([4, 8]);
+  expect(battleship.isSunk()).toBeFalsy();
+  expect(submarine.isSunk()).toBeFalsy();
 });
 
 test("Ships being sunk", () => {
-    battleship.hit(1);
-    battleship.hit(2);
-    battleship.hit(3);
-    battleship.hit(4);
-    submarine.hit(1);
-    submarine.hit(2);
-    submarine.hit(3);
-    expect(battleship.isSunk()).toBeTruthy();
-    expect(submarine.isSunk()).toBeTruthy();
+  battleship.hit([1, 3]);
+  battleship.hit([1, 4]);
+  battleship.hit([1, 5]);
+  battleship.hit([1, 6]);
+  submarine.hit([3, 5]);
+  submarine.hit([4, 5]);
+  submarine.hit([5, 5]);
+  submarine.hit([6, 5]);
+  expect(battleship.isSunk()).toBeTruthy();
+  expect(submarine.isSunk()).toBeTruthy();
 });
 
 test("Ships being hit in the same spot", () => {
-    battleship.hit(1);
-    battleship.hit(1);
-    battleship.hit(1);
-    battleship.hit(1);
-    submarine.hit(1);
-    submarine.hit(1);
-    submarine.hit(1);
-    expect(battleship.hits.length).toBe(1);
-    expect(submarine.hits.length).toBe(1);
+  battleship.hit([0, 0]);
+  battleship.hit([0, 0]);
+  submarine.hit([1, 1]);
+  submarine.hit([1, 1]);
+  expect(battleship.hits.length).toBe(1);
+  expect(submarine.hits.length).toBe(1);
 });
