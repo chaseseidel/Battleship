260e428fc0c753b5475d9303038e9afa2ac02e3e
chaseseidel <chaseseidel16@gmail.com>
2023-07-30 21:43:46 -0700

    Create player class with turn and randomShip functions

---
+++ src/modules/player.js
@@ -0,0 +1,51 @@
+import Gameboard from "./gameboard";
+import Ship from "./ship";
+
+export default class Player {
+  constructor(name) {
+    this.name = name;
+    this.board = new Gameboard();
+    this.turn = false;
+  }
+
+  isTurn() {
+    return this.turn;
+  }
+
+  changeTurn() {
+    if (this.turn) {
+      this.turn = false;
+    } else {
+      this.turn = true;
+    }
+  }
+
+  randomShip(length) {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+    let direction;
+
+    let odds = Math.floor(Math.random() * 10);
+    if (odds < 5) {
+      direction = "x";
+    } else {
+      direction = "y";
+    }
+
+    try {
+      this.board.placeShip(new Ship(length), [x, y], direction);
+    } catch (e) {
+      this.randomShip(length);
+    }
+  }
+
+  randomFleet() {
+    for (let i = 0; i < 2; i++) {
+      this.randomShip(3);
+    }
+
+    this.randomShip(2);
+    this.randomShip(4);
+    this.randomShip(5);
+  }
+}
