58b6a22e2fbf9fad35cdf662e5faa27504a7e8df
chaseseidel <chaseseidel16@gmail.com>
2023-07-31 23:53:14 -0700

    Create ship placement

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -50,4 +50,92 @@ export default class DOM {
       gameContainer.appendChild(board);
     }
   }
+
+  static placeShip(tiles, length, position, direction) {
+    position = position[1] * 10 + position[0];
+    if (direction === "x") {
+      for (let i = 0; i < length; i++) {
+        tiles[position + i].classList.add("ship");
+      }
+    } else {
+      for (let i = 0; i < length; i++) {
+        tiles[position + 10 * i].classList.add("ship");
+      }
+    }
+  }
+
+  static markAttack(tiles, position) {
+    position = position[1] * 10 + position[0];
+
+    if (tiles[position].classList.contains("ship")) {
+      tiles[position].className = "tile hit";
+    } else {
+      tiles[position].classList.add("miss");
+    }
+  }
+
+  static playerPlacesShip(tiles, length, direction, shipLengths, ships) {
+    tiles.forEach((tile, index) => {
+      tile.addEventListener("click", () => {
+        let interfere = false;
+        if (direction === "x") {
+          for (let i = 0; i < length; i++) {
+            if ((index % 10) + length > 10) {
+              interfere = true;
+              break;
+            }
+            if (tiles[index + i].classList.contains("ship")) {
+              interfere = true;
+              break;
+            }
+          }
+          if (!interfere) {
+            DOM.placeShip(
+              tiles,
+              length,
+              [index % 10, Math.floor(index / 10)],
+              direction
+            );
+            shipLengths.shift();
+            ships.push([
+              length,
+              [index % 10, Math.floor(index / 10)],
+              direction,
+            ]);
+          }
+        } else {
+          for (let i = 0; i < length; i++) {
+            if (Math.floor(index / 10) + length > 10) {
+              interfere = true;
+              break;
+            }
+            if (tiles[index + 10 * i].classList.contains("ship")) {
+              interfere = true;
+              break;
+            }
+          }
+          if (!interfere) {
+            DOM.placeShip(
+              tiles,
+              length,
+              [index % 10, Math.floor(index / 10)],
+              direction
+            );
+            shipLengths.shift();
+            ships.push([
+              length,
+              [index % 10, Math.floor(index / 10)],
+              direction,
+            ]);
+          }
+        }
+      });
+    });
+  }
+
+  static removeEventListeners(tiles) {
+    tiles.forEach((tile) => {
+      tile.parentNode.replaceChild(tile.cloneNode(true), tile);
+    });
+  }
 }
