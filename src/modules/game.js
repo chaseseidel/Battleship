f004d98701bb499b4bed9a0575e2bf622677884b
chaseseidel <chaseseidel16@gmail.com>
2023-08-03 20:52:35 -0700

    Complete working game and pregame functions

--- src/modules/game.js
+++ src/modules/game.js
@@ -2,71 +2,11 @@ import Player from "./player";
 import Ship from "./ship";
 import DOM from "./dom";
 
-const shipLengths = [2, 3, 3, 4, 5];
+let shipLengths = [2, 3, 3, 4, 5];
 
-const playerShips = [];
+let playerShips = [];
 
 let direction = "x";
-
-// export default function playGame() {
-//   const player = new Player("Bob");
-//   const computer = new Player("AI");
-
-//   player.board.createBoard();
-//   computer.board.createBoard();
-
-//   player.changeTurn();
-
-//   testShips1.forEach((ship, index) => {
-//     player.board.placeShip(ship, testPositions[index], testDirections[index]);
-//   });
-
-//   testShips2.forEach((ship, index) => {
-//     computer.board.placeShip(ship, testPositions[index], testDirections[index]);
-//   });
-
-//   player.board.printBoard();
-//   computer.board.printBoard();
-
-//   while (!player.board.allShipsSunk() && !computer.board.allShipsSunk()) {
-//     if (player.board.allShipsSunk() && !computer.board.allShipsSunk()) {
-//       console.log("Potential draw");
-//       player.randomAttack(computer);
-//       if (computer.board.allShipsSunk()) {
-//         console.log("It's a draw!");
-//       }
-//       break;
-//     }
-
-//     if (!player.board.allShipsSunk() && computer.board.allShipsSunk()) {
-//       console.log("Potential draw");
-//       computer.randomAttack(player);
-//       if (player.board.allShipsSunk()) {
-//         console.log("It's a draw!");
-//       }
-//       break;
-//     }
-
-//     if (player.isTurn()) {
-//       player.randomAttack(computer);
-//       player.changeTurn();
-//       computer.changeTurn();
-//     } else {
-//       computer.randomAttack(player);
-//       computer.changeTurn();
-//       player.changeTurn();
-//     }
-//   }
-
-//   if (computer.board.allShipsSunk()) {
-//     console.log("Player won");
-//   } else {
-//     console.log("Computer won");
-//   }
-
-//   player.board.printBoard();
-//   computer.board.printBoard();
-// }
 
 export default function playGame() {
   const player = new Player("Bob");
@@ -91,61 +31,33 @@ export default function playGame() {
 
   computerTiles.forEach((tile, index) => {
     tile.addEventListener("click", () => {
-      if (
-        !tile.classList.contains("miss") &&
-        !tile.classList.contains("hit")
-      ) {
+      if (!tile.classList.contains("miss") && !tile.classList.contains("hit")) {
         const position = [index % 10, Math.floor(index / 10)];
         player.attackPlayer(computer, position);
         DOM.markPlayerAttack(computerTiles, position, computer);
         computer.randomAttack(player);
         const attack = getAttack(player);
         DOM.markAttack(playerTiles, attack);
+        if (computer.board.allShipsSunk()) {
+          DOM.toggleGameOverModal();
+        } else if (player.board.allShipsSunk()) {
+          DOM.toggleGameOverModal();
+          const winnerText = document.getElementById("winner-text");
+          winnerText.textContent = "You lost the game!";
+        }
       }
     });
   });
 
-  //   while (!player.board.allShipsSunk() && !computer.board.allShipsSunk()) {
-  //     if (player.board.allShipsSunk() && !computer.board.allShipsSunk()) {
-  //       console.log("Potential draw");
-  //       player.randomAttack(computer);
-  //       if (computer.board.allShipsSunk()) {
-  //         console.log("It's a draw!");
-  //       }
-  //       break;
-  //     }
-
-  //     if (!player.board.allShipsSunk() && computer.board.allShipsSunk()) {
-  //       console.log("Potential draw");
-  //       const attack = computer.randomAttack(player);
-  //       DOM.markAttack(playerTiles, attack);
-  //       if (player.board.allShipsSunk()) {
-  //         console.log("It's a draw!");
-  //       }
-  //       break;
-  //     }
-
-  //     if (player.isTurn()) {
-  //       player.randomAttack(computer);
-  //       player.changeTurn();
-  //       computer.changeTurn();
-  //     } else {
-  //       computer.randomAttack(player);
-  //       const attack = getAttack(player);
-  //       DOM.markAttack(playerTiles, attack);
-  //       computer.changeTurn();
-  //       player.changeTurn();
-  //     }
-  //   }
-
-  //   if (computer.board.allShipsSunk()) {
-  //     console.log("Player won");
-  //   } else {
-  //     console.log("Computer won");
-  //   }
-
-  //   player.board.printBoard();
-  //   computer.board.printBoard();
+  const resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", () => {
+    DOM.resetPage();
+    DOM.loadPage();
+    shipLengths = [2, 3, 3, 4, 5];
+    playerShips = [];
+    direction = "x";
+    preGame();
+  });
 }
 
 export function preGame() {
@@ -238,8 +150,9 @@ export function preGame() {
     DOM.addInteractiveBoard(fakePlayerTiles, shipLengths[0], direction);
     if (shipLengths.length < 1) {
       DOM.toggleFakeModal();
-      const start = document.getElementById("start-game");
-      start.addEventListener("click", () => {
+      const rotate = document.getElementById("rotate");
+      rotate.textContent = "Start Game";
+      rotate.addEventListener("click", () => {
         playGame();
         DOM.updateBoard();
         DOM.toggleModal();
